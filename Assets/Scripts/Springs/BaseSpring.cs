80a0f1be1c9a7f6ac9f7d211e3d6717ba48fe163
Pasha Stierle <pasha.king.stierle@gmail.com>
2026-03-27 16:38:19 -0700

    Updated project to latest LTS
    also reorganized script folder and started work on springs library, also device swapping.

---
+++ Assets/Scripts/Springs/BaseSpring.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace CustomSprings
+{
+    public abstract class BaseSpring<T>
+    {
+        // Defaults
+        public virtual float Strength { get; set; } = 169f;
+        public virtual float Damping { get; set; } = 26f;
+        public virtual float Mass { get; set; } = 1f;
+        public virtual T StartValue { get; set; }
+        public virtual T EndValue { get; set; }
+        public virtual T CurrentValue { get; set; }
+        public virtual T InitialVelocity { get; set; }
+        public virtual T CurrentVelocity { get; set; }
+
+        public abstract void Reset();
+
+        public virtual void UpdateEndValue(T Value) => UpdateEndValue(Value, CurrentVelocity);
+
+        public abstract void UpdateEndValue(T Value, T Velocity);
+
+        public abstract T Update(float deltaTime);
+    }
+}
